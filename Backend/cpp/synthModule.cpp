373e1d31eb0387c598bd55f408d8da065f5adec5
przemekkojs <przemek.kojs@gmail.com>
2026-03-19 14:10:21 +0100

    Synth module implementation

--- Backend/cpp/synthModule.cpp
+++ Backend/cpp/synthModule.cpp
@@ -1,1 +1,7 @@
 #include "../h/synthModule.h"
+
+synthModule::synthModule(std::shared_ptr<voices> voiceManager) : module(voiceManager) { }
+
+audioSignal& synthModule::play(noteSignal& signal) {
+
+}
