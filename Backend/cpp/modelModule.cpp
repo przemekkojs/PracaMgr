fda808a12f5321507041a451b1c9a074bf0d2fb4
przemekkojs <przemek.kojs@gmail.com>
2026-03-19 14:08:09 +0100

    Model module implementation

--- Backend/cpp/modelModule.cpp
+++ Backend/cpp/modelModule.cpp
@@ -1,1 +1,7 @@
 #include "../h/modelModule.h"
+
+modelModule::modelModule(std::shared_ptr<voices> voiceManager) : module(voiceManager) { }
+
+audioSignal& modelModule::play(noteSignal& signal) {
+	return EMPTY_AUDIO_SIGNAL;
+}
