0000f38cfbf6e85635d30efc1aff7e9c2d789247
przemekkojs <przemek.kojs@gmail.com>
2026-03-19 14:09:07 +0100

    Samples module implementation

--- Backend/cpp/samplesModule.cpp
+++ Backend/cpp/samplesModule.cpp
@@ -1,1 +1,7 @@
 #include "../h/samplesModule.h"
+
+samplesModule::samplesModule(std::shared_ptr<voices> voiceManager) : module(voiceManager) { }
+
+audioSignal& samplesModule::play(noteSignal& signal) {
+
+}
