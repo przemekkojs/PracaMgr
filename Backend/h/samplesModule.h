288a300bb2ba9b1547c1ce2d35e817f126dee9fc
przemekkojs <przemek.kojs@gmail.com>
2026-03-23 19:08:31 +0100

    Revert "Reset"
    This reverts commit c59854595e1ea544d710819d56c2da751ce63c39.

--- Backend/h/samplesModule.h
+++ Backend/h/samplesModule.h
@@ -4,20 +4,55 @@
 #include "../lib/miniaudio.h"
 
 #include <map>
-#include <vector>
-#include <memory>
+#include <queue>
 
+struct LoopingSample {
+    ma_data_source_base base;
+
+    float* data;
+    ma_uint64 frameCount;
+    ma_uint32 channels;
+    ma_uint32 sampleRate;
+
+    ma_uint64 cursor;
+    ma_uint64 loopStart;
+    ma_uint64 loopEnd;
+    ma_uint64 fadeLength;
+
+    bool loopActive;
+    ma_uint64 fadeCursor;
+};
+
+struct sampleSet {
+    ma_sound attack;
+    ma_sound sustain1;
+    ma_sound sustain2;
+    ma_sound release;
+
+    LoopingSample looping;
+
+    bool useFirst = true;
+    float crossfadePos = 0.0f;
+    float crossfadeStep = 1.0f / 4096.0f;
+
+    void startLoop();
+    void stopLoop();
+    void updateLooping();
+};
 
 class samplesModule : public module {
 public:
-    samplesModule(std::shared_ptr<voices> voiceManager, int maxPolyphony = 1024);
-    ~samplesModule();
+	samplesModule(std::shared_ptr<voices> voiceManager, int maxPolyphony=1024);
+	~samplesModule();
+	void play(const noteSignal& signal, audioSignal& output) override;
+	void initEngine();
 
-    void play(const noteSignal& signal, audioSignal& output) override;
-    void initEngine();
-
+	const std::map<std::pair<int, int>, sampleSet*>& getSamples() const { return this->samples; }
 private:
-    std::map<std::pair<int, int>, int> samples;
+	std::map<std::pair<int, int>, sampleSet*> samples;
+	ma_engine engine;
+    ma_device device;
 
-    int maxPolyphony;
+	int maxPolyphony;
 };
+
